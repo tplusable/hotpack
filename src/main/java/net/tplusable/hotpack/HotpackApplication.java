24873203cdd4b266bd48ccc582fb4e05bb7b93c6
chlorella71 <chlorella71@naver.com>
2024-12-10 17:42:56 +0900

    init:프로젝트 초기화 spring boot3

---
+++ src/main/java/net/tplusable/hotpack/HotpackApplication.java
@@ -0,0 +1,13 @@
+package net.tplusable.hotpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotpackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/tplusable/hotpack/HotpackApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tplusable.hotpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotpackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
