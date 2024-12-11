3b1ef3710b92b93c316ae24bc03a4b9c89da81ab
chlorella71 <chlorella71@naver.com>
2024-12-11 11:03:49 +0900

    fix: 오류 해결을 위한 설정파일 덮어쓰기

---
+++ src/main/java/net/tplusable/hotpack/user/SiteUser.java
@@ -0,0 +1,24 @@
+package net.tplusable.hotpack.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class SiteUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String userId;
+    @Column(nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String name;
+}
